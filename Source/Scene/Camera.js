e919c8a6b6e61a7865885c70b767123dd0a7d252
Mike McGann <mike.mcgann@nasa.gov>
2015-04-06 11:51:44 -0400

    Typo correction in Camera documentation

--- Source/Scene/Camera.js
+++ Source/Scene/Camera.js
@@ -2202,7 +2202,7 @@ define([
      *
      * @param {Object} options Object with the following properties:
      * @param {Cartesian3|Rectangle} options.destination The final position of the camera in WGS84 (world) coordinates or a rectangle that would be visible from a top-down view.
-     * @param {Object} [options.orientation] An object that contains either direction and up properties or heading, pith and roll properties. By default, the direction will point
+     * @param {Object} [options.orientation] An object that contains either direction and up properties or heading, pitch and roll properties. By default, the direction will point
      * towards the center of the frame in 3D and in the negative z direction in Columbus view or 2D. The up direction will point towards local north in 3D and in the positive
      * y direction in Columbus view or 2D.
      * @param {Number} [options.duration=3.0] The duration of the flight in seconds.
